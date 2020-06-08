ac1c222da66aeb966aa63a2eddbca42bf1cdf1ef
yumingtao <yumingtao2008@gmail.com>
2020-06-08 15:49:17 +0800

    Refactoring.

--- src/main/java/com/ymt/example/model/JwtHeader.java
+++ src/main/java/com/ymt/example/model/JwtHeader.java
@@ -1,6 +1,5 @@
 package com.ymt.example.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
